5aaa39fa022fd2ef929044bc29235a43b0be395c
(Brian O. Blaza) <151626255+Seiyaaaaaaaaaaaa@users.noreply.github.com>
2024-10-14 07:44:57 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="register.css"> 
+</head>
+<body>
+    <h1>Be Part of the Group 4</h1>
